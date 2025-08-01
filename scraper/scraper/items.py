e77344911a191d955ee4e83df8a94f048cd1d666
hamna-akhtar <hamna.akhtar@ignicube.com>
2025-08-01 14:52:01 +0500

    complete adscraper

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -5,13 +5,6 @@
 
 import scrapy
 
-
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
-
-
 class AdItem(scrapy.Item):
     url = scrapy.Field()
     city = scrapy.Field()
